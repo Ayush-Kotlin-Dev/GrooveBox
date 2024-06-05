34b0e4ddc9ad5ca7021444b88788feca49f240ba
Ayushrai2005 <shweetarai@gmail.com>
2024-06-05 13:41:39 +0530

    Completed the SeekBar Action(Slidable) , with Next and Previous Track Manager

--- app/src/main/java/ayush/ggv/groovebox/MainActivity.kt
+++ app/src/main/java/ayush/ggv/groovebox/MainActivity.kt
@@ -1,44 +1,75 @@
 package ayush.ggv.groovebox
 
 import android.annotation.SuppressLint
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.luminance
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.fontscaling.MathUtils.lerp
+import androidx.compose.ui.unit.sp
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
 import ayush.ggv.groovebox.ui.theme.GrooveBoxTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import kotlinx.coroutines.delay
@@ -46,10 +77,13 @@ import kotlin.math.absoluteValue
 
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("RestrictedApi")
-    @OptIn(ExperimentalFoundationApi::class)
+    lateinit var player: ExoPlayer
+
+    @SuppressLint("RestrictedApi", "UnusedContentLambdaTargetStateParameter")
+    @OptIn(ExperimentalFoundationApi::class, ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        player = ExoPlayer.Builder(this).build()
         enableEdgeToEdge()
         setContent {
             GrooveBoxTheme {
@@ -142,7 +176,74 @@ class MainActivity : ComponentActivity() {
                         Music("Music 2", R.raw.music2, R.drawable.music2),
                         Music("Music 3", R.raw.music33, R.drawable.music3)
                     )
-                    val pagerState = rememberPagerState(pageCount = { musics.count()})
+                    val pagerState = rememberPagerState(pageCount = { musics.count() })
+                    val playingIndex = remember { mutableIntStateOf(0) }
+
+
+                    LaunchedEffect(pagerState.currentPage) {
+                        playingIndex.value = pagerState.currentPage
+                        player.seekTo(pagerState.currentPage , 0)
+
+                    }
+
+
+                    LaunchedEffect(Unit) {
+                        musics.forEach {
+                            val path = "android.resource://$packageName/${it.music}"
+                            val mediaItem = MediaItem.fromUri(Uri.parse(path))
+                            player.addMediaItem(mediaItem)
+                        }
+                    }
+                    player.prepare()
+
+                    val playing = remember {
+                        mutableStateOf(false)
+                    }
+                    val currentPosition = remember {
+                        mutableLongStateOf(0)
+                    }
+                    val totalDuration = remember {
+                        mutableLongStateOf(0)
+                    }
+                    val progressSize = remember {
+                        mutableStateOf(
+                            IntSize(
+                                width = 0,
+                                height = 0
+                            )
+                        )
+                    }
+
+
+                    LaunchedEffect(player.isPlaying) {
+                        playing.value = player.isPlaying
+                    }
+
+                    LaunchedEffect(
+                        player.currentPosition
+                    ) {
+                        currentPosition.value = player.currentPosition
+                    }
+                    LaunchedEffect(
+                        player.duration
+                    ) {
+                        if (
+                            player.duration > 0
+                        ) {
+                            totalDuration.value = player.duration
+                        }
+                    }
+                    LaunchedEffect(player.currentMediaItemIndex) {
+                        playingIndex.value = player.currentMediaItemIndex
+                        pagerState.animateScrollToPage(playingIndex.value , animationSpec = tween(2000))
+
+                    }
+                    var percentReached = currentPosition.value.toFloat() / (if(totalDuration.value > 0) totalDuration.value else 0).toFloat()
+
+                    if(percentReached.isNaN()){
+                       percentReached = 0f
+                    }
+
                     Box(
                         modifier = Modifier
                             .fillMaxSize()
@@ -154,22 +255,35 @@ class MainActivity : ComponentActivity() {
                                     )
                                 )
                             ),
-                        contentAlignment = androidx.compose.ui.Alignment.Center
+                        contentAlignment = Alignment.Center
                     ) {
                         val configuration = LocalConfiguration.current
 
                         Column(
                             horizontalAlignment = Alignment.CenterHorizontally
                         ) {
+                            val textColor by animateColorAsState(
+                                targetValue = if (animatedColor.luminance() > .5f) Color(
+                                    0xff414141
+                                ) else Color.White,
+                                animationSpec = tween(2000)
+                            )
+                            AnimatedContent(targetState = playingIndex.value, transitionSpec = {
+                                (scaleIn() + fadeIn()) togetherWith (scaleOut() + fadeOut())
+                            }) {
+                                Text(text = musics[it].name, fontSize = 52.sp, color = textColor)
+                            }
+                            Spacer(modifier = Modifier.height(30.dp))
+
                             HorizontalPager(
                                 modifier = Modifier.fillMaxWidth(),
                                 state = pagerState,
-                                pageSize = PageSize.Fixed((configuration.screenWidthDp/(1.7)).dp),
+                                pageSize = PageSize.Fixed((configuration.screenWidthDp / (1.7)).dp),
                                 contentPadding = PaddingValues(horizontal = 85.dp)
-                            ) {page->
+                            ) { page ->
                                 Card(
                                     modifier = Modifier
-                                        .size((configuration.screenWidthDp /(1.7)).dp)
+                                        .size((configuration.screenWidthDp / (1.7)).dp)
                                         .graphicsLayer {
                                             val pageOffset = (
                                                     (pagerState.currentPage - page) +
@@ -196,7 +310,7 @@ class MainActivity : ComponentActivity() {
                                             shape = CircleShape
                                         )
                                         .padding(6.dp),
-                                  shape  = CircleShape
+                                    shape = CircleShape
                                 ) {
                                     Image(
                                         painter = painterResource(id = musics[page].cover),
@@ -206,11 +320,91 @@ class MainActivity : ComponentActivity() {
                                     )
                                 }
 
+                            }
+                            Spacer(modifier = Modifier.height(30.dp))
+                            Row(
+                                modifier = Modifier.height(50.dp),
+                                verticalAlignment = Alignment.CenterVertically
+                            )
+                            {
+                                Text(
+                                    text =  convertLongToText(currentPosition.value),
+                                    modifier = Modifier.width(55.dp),
+                                    color = textColor,
+                                    textAlign = androidx.compose.ui.text.style.TextAlign.Center
+                                )
+                                //progress Box
+                                Box(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .weight(1f)
+                                        .height(8.dp)
+                                        .padding(horizontal = 10.dp)
+                                        .background(Color.White.copy(alpha = 0.5f), CircleShape)
+                                        .clip(CircleShape)
+                                        .onGloballyPositioned {
+                                            progressSize.value = IntSize(it.size.width, it.size.height)
+                                        }
+                                        .pointerInput(Unit) {
+                                            detectHorizontalDragGestures { change, dragAmount ->
+                                                change.consume()
+                                                val totalWidth = progressSize.value.width.toFloat()
+                                                val percent = dragAmount / totalWidth
+                                                val seekTo =
+                                                    (currentPosition.value + (percent * totalDuration.value)).coerceIn(
+                                                        0f,
+                                                        totalDuration.value.toFloat()
+                                                    ).toLong()
+                                                player.seekTo(seekTo)
+                                            }
+                                        }
+
+                                )
+                                {
+                                    Box(
+                                        modifier = Modifier
+                                            .fillMaxWidth(fraction = if(playing.value) percentReached else 0f)
+                                            .fillMaxHeight()
+                                            .clip(RoundedCornerShape(8.dp))
+                                            .background(Color(0xFF414141)),
+                                    )
+
+                                }
+
+                                Text(
+                                    text = convertLongToText(totalDuration.value),
+                                    modifier = Modifier.width(55.dp),
+                                    color = textColor,
+                                    textAlign = androidx.compose.ui.text.style.TextAlign.Center
+                                )
+                            }
+
+                            Spacer(modifier = Modifier.height(24.dp))
+                            Row(
+                                horizontalArrangement = Arrangement.SpaceEvenly,
+                                verticalAlignment = Alignment.CenterVertically,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = 32.dp)
+                            ) {
+                                Control(icon = R.drawable.ic_fast_rewind, size = 60.dp) {
+                                    player.seekToPreviousMediaItem()
+                                }
+                                Control(icon =if( playing.value ) R.drawable.ic_pause else R.drawable.ic_play , size = 80.dp) {
+                                    //play music
+                                    if (playing.value) {
+                                        player.pause()
+                                    } else {
+                                        player.play()
+                                    }
+                                }
+                                Control(icon = R.drawable.ic_fast_forward, size = 60.dp) {
+                                    player.seekToNextMediaItem()
+                                }
 
                             }
+
                         }
-
-
                     }
                 }
             }
@@ -218,9 +412,35 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun Control(icon: Int, size: Dp, onClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .size(size)
+            .clip(CircleShape)
+            .clickable { onClick() }
+            .background(Color.White.copy(alpha = 0.5f)),
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            modifier = Modifier.size(size / 2),
+            painter = painterResource(id = icon),
+            contentDescription = null,
+            tint = Color(0xff414141)
+        )
+    }
+
+}
+fun convertLongToText(time: Long): String {
+    val seconds = time / 1000
+    val minutes = seconds / 60
+    val remainingSeconds = seconds % 60
+    return "${if (minutes < 10) "0" else ""}$minutes:${if (remainingSeconds < 10) "0" else ""}$remainingSeconds"
+}
+
+
 data class Music(
     val name: String,
     val music: Int,
     val cover: Int
 )
-
--- app/src/main/java/ayush/ggv/groovebox/ui/theme/Type.kt
+++ app/src/main/java/ayush/ggv/groovebox/ui/theme/Type.kt
@@ -2,14 +2,18 @@ package ayush.ggv.groovebox.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import ayush.ggv.groovebox.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily(listOf(
+            Font(R.font.pusia)
+        )),
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
